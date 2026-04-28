f5070a02f10fe348dc0c04d77b61fd9fff131270
James McDermott <james.j.mcdermott@ericsson.com>
2026-04-28 15:07:43 +0100

    Feat: add package resource file sizes to PackageRevision interface
    - on ClosePackageRevisionDraft:
      - add up individual sizes of PackageRevisionResources
      - store total as a separate metadata field in package_revisions
        table
    - on get/list PackageRevision, include total resources size in status
      field of returned API object
    
    https://github.com/nephio-project/porch/issues/811
    Signed-off-by: James McDermott <james.j.mcdermott@ericsson.com>

--- test/e2e/api/rpkg_init_test.go
+++ test/e2e/api/rpkg_init_test.go
@@ -1,4 +1,4 @@
-// Copyright 2025 The kpt Authors
+// Copyright 2025-2026 The kpt Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,10 +17,13 @@ package api
 import (
 	"github.com/google/go-cmp/cmp"
 	kptfilev1 "github.com/kptdev/kpt/pkg/api/kptfile/v1"
-	porchapi "github.com/kptdev/porch/api/porch/v1alpha1"
-	suiteutils "github.com/kptdev/porch/test/e2e/suiteutils"
+	porchapi "github.com/nephio-project/porch/api/porch/v1alpha1"
+	suiteutils "github.com/nephio-project/porch/test/e2e/suiteutils"
+	"github.com/stretchr/testify/assert"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+var defaultEmptyResourcesSize = 893
 
 func (t *PorchSuite) TestInitEmptyPackage() {
 	// Create a new package via init, no task specified
@@ -40,6 +43,8 @@ func (t *PorchSuite) TestInitEmptyPackage() {
 	t.validateKptFileMetadata(pr, packageName, &kptfilev1.PackageInfo{
 		Description: description,
 	})
+
+	assert.Equal(t, defaultEmptyResourcesSize, pr.Status.PrrSizeOnDisk)
 }
 
 func (t *PorchSuite) TestInitTaskPackage() {
