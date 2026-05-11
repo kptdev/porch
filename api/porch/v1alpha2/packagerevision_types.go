fe642351763014e4b341ada16de3888c9722dc66
liamfallon <liam.fallon@est.tech>
2026-05-11 17:56:52 +0100

    Address CoPilot comments
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- api/porch/v1alpha2/packagerevision_types.go
+++ api/porch/v1alpha2/packagerevision_types.go
@@ -205,6 +205,7 @@ type PackageRevisionStatus struct {
 // PackageSource specifies how a package was created.
 // Exactly one field must be set.
 // +kubebuilder:validation:XValidation:rule="[has(self.init), has(self.clone), has(self.copyFrom), has(self.upgrade)].filter(x, x).size() == 1",message="exactly one of init, clone, copyFrom, or upgrade must be set"
+// +kubebuilder:validation:XValidation:rule="!has(self.clone) || has(self.clone.cloneFrom)",message="clone.cloneFrom must be set when clone is specified"
 type PackageSource struct {
 	// Init creates a brand new package from scratch.
 	Init *PackageInitSpec `json:"init,omitempty"`
