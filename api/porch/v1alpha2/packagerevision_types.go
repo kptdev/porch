7773ebe93238c7f74effbb4ba1755fce9e48115a
liamfallon <liam.fallon@est.tech>
2026-05-12 13:39:37 +0100

    Addressed copilot comments
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- api/porch/v1alpha2/packagerevision_types.go
+++ api/porch/v1alpha2/packagerevision_types.go
@@ -206,6 +206,7 @@ type PackageRevisionStatus struct {
 // Exactly one field must be set.
 // +kubebuilder:validation:XValidation:rule="[has(self.init), has(self.clone), has(self.copyFrom), has(self.upgrade)].filter(x, x).size() == 1",message="exactly one of init, clone, copyFrom, or upgrade must be set"
 // +kubebuilder:validation:XValidation:rule="!has(self.clone) || has(self.clone.cloneFrom)",message="clone.cloneFrom must be set when clone is specified"
+// +kubebuilder:validation:XValidation:rule="!has(self.clone) || has(self.clone.cloneFrom.upstreamRef) || has(self.clone.cloneFrom.git)",message="clone.cloneFrom must specify either upstreamRef or git when clone is specified"
 type PackageSource struct {
 	// Init creates a brand new package from scratch.
 	Init *PackageInitSpec `json:"init,omitempty"`
