399105ffabcd1d36f646bff618c0bf0e3035a55c
liamfallon <liam.fallon@est.tech>
2026-06-04 08:48:09 +0100

    Fix upgrade tests so they work in 1.5
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- pkg/task/upgrade_test.go
+++ pkg/task/upgrade_test.go
@@ -23,12 +23,7 @@ import (
 	porchapi "github.com/kptdev/porch/api/porch/v1alpha1"
 	"github.com/kptdev/porch/pkg/externalrepo/fake"
 	"github.com/kptdev/porch/pkg/repository"
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
-)
-
-var (
-	res = &fakeReferenceResolver{}
 )
 
 func createMockedResources() repository.PackageResources {
@@ -68,93 +63,6 @@ func TestApplyErrorInvalidUpstreamUprade(t *testing.T) {
 	_, _, err := mutation.apply(ctx, resources)
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "error fetching the resources for package")
-}
-
-func TestUpgradeToPlaceholderAsNewUpstream(t *testing.T) {
-	repositoryNamespace := "test-namespace"
-	repositoryName := "repo"
-	pkg := "1234567890"
-	revision := -1
-	workspace := "main"
-	placeholderPackageName := "repo.1234567890.main"
-	placeholderPackageRevision := &fake.FakePackageRevision{
-		PrKey: repository.PackageRevisionKey{
-			PkgKey: repository.PackageKey{
-				RepoKey: repository.RepositoryKey{
-					Namespace: repositoryNamespace,
-					Name:      repositoryName,
-				},
-				Package: pkg,
-			},
-			Revision:      revision,
-			WorkspaceName: workspace,
-		},
-		PackageLifecycle: porchapi.PackageRevisionLifecyclePublished,
-		Resources: &porchapi.PackageRevisionResources{
-			Spec: porchapi.PackageRevisionResourcesSpec{
-				PackageName:    pkg,
-				Revision:       revision,
-				RepositoryName: repositoryName,
-				Resources: map[string]string{
-					kptfilev1.KptFileName: strings.TrimSpace(`
-apiVersion: kpt.dev/v1
-kind: Kptfile
-metadata:
-  name: example
-  annotations:
-    config.kubernetes.io/local-config: "true"
-info:
-  description: sample description
-					`),
-				},
-			},
-		},
-	}
-
-	oldRev := *placeholderPackageRevision
-	oldPackageRevision := &oldRev
-	oldPackageRevision.PrKey.WorkspaceName = "old"
-
-	repo := &fake.Repository{
-		PackageRevisions: []repository.PackageRevision{
-			oldPackageRevision,
-			placeholderPackageRevision,
-		},
-	}
-	repoOpener := &fakeRepositoryOpener{
-		repository: repo,
-	}
-
-	mutation := upgradePackageMutation{
-		upgradeTask: &porchapi.Task{
-			Type: porchapi.TaskTypeUpgrade,
-			Upgrade: &porchapi.PackageUpgradeTaskSpec{
-				OldUpstream: porchapi.PackageRevisionRef{
-					Name: "repo.1234567890.old",
-				},
-				NewUpstream: porchapi.PackageRevisionRef{
-					Name: placeholderPackageName,
-				},
-				LocalPackageRevisionRef: porchapi.PackageRevisionRef{
-					Name: "destination",
-				},
-			},
-		},
-
-		namespace:         "test-namespace",
-		pkgName:           pkg,
-		referenceResolver: res,
-		repoOpener:        repoOpener,
-	}
-	_, _, err := mutation.apply(context.Background(), repository.PackageResources{})
-	assert.ErrorContains(t, err, "placeholder package revision", "Expected error upgrading to the placeholder package revision as new upstream")
-
-	mutation.referenceResolver = (&errorAfterNReferenceResolver{r: res, err: errors.New("error resolving reference")}).startAt(2)
-
-	_, _, err = mutation.apply(context.Background(), repository.PackageResources{})
-	assert.ErrorContains(t, err, "failed to resolve repository reference")
-
-	mutation.referenceResolver = res
 }
 
 func TestUpgradeSubpackageDir(t *testing.T) {
@@ -279,8 +187,8 @@ info:
 			},
 			namespace:         repositoryNamespace,
 			pkgName:           pkg,
-			referenceResolver: res,
 			repoOpener:        &fakeRepositoryOpener{repository: repo},
+			referenceResolver: &fakeReferenceResolver{},
 		}
 
 		_, _, err := mutation.apply(context.Background(), repository.PackageResources{})
@@ -340,15 +248,15 @@ info:
 			},
 			namespace:         repositoryNamespace,
 			pkgName:           pkg,
-			referenceResolver: res,
 			repoOpener:        &fakeRepositoryOpener{repository: repo},
+			referenceResolver: &fakeReferenceResolver{},
 		}
 
 		result, taskResult, err := mutation.apply(context.Background(), repository.PackageResources{})
 		assert.NoError(t, err)
 		assert.NotNil(t, taskResult)
 		// The result should contain the merged resources based on the subpackage content only,
-		// not the root-level resources or prefixed paths
+		// not the root-level resources or prefixed pathsExpand commentComment on line R351Resolved
 		assert.Contains(t, result.Contents, kptfilev1.KptFileName)
 		assert.Contains(t, result.Contents, "subpkg-resource.yaml")
 		assert.NotEmpty(t, result.Contents["subpkg-resource.yaml"])
@@ -407,104 +315,12 @@ info:
 			},
 			namespace:         repositoryNamespace,
 			pkgName:           pkg,
-			referenceResolver: res,
 			repoOpener:        &fakeRepositoryOpener{repository: repo},
+			referenceResolver: &fakeReferenceResolver{},
 		}
 
 		_, _, err := mutation.apply(context.Background(), repository.PackageResources{})
 		assert.Error(t, err)
 		assert.Contains(t, err.Error(), `subpackage "my-subpkg" not found in package`)
 	})
-}
-
-func TestUpgradePlaceholderAsLocal(t *testing.T) {
-	repositoryNamespace := "test-namespace"
-	repositoryName := "repo"
-	pkg := "1234567890"
-	revision := -1
-	workspace := "main"
-	placeholderPackageName := "repo.1234567890.main"
-	placeholderPackageRevision := &fake.FakePackageRevision{
-		PrKey: repository.PackageRevisionKey{
-			PkgKey: repository.PackageKey{
-				RepoKey: repository.RepositoryKey{
-					Namespace: repositoryNamespace,
-					Name:      repositoryName,
-				},
-				Package: pkg,
-			},
-			Revision:      revision,
-			WorkspaceName: workspace,
-		},
-		PackageLifecycle: porchapi.PackageRevisionLifecyclePublished,
-		Resources: &porchapi.PackageRevisionResources{
-			Spec: porchapi.PackageRevisionResourcesSpec{
-				PackageName:    pkg,
-				Revision:       revision,
-				RepositoryName: repositoryName,
-				Resources: map[string]string{
-					kptfilev1.KptFileName: strings.TrimSpace(`
-apiVersion: kpt.dev/v1
-kind: Kptfile
-metadata:
-  name: example
-  annotations:
-    config.kubernetes.io/local-config: "true"
-info:
-  description: sample description
-					`),
-				},
-			},
-		},
-	}
-
-	oldRev := *placeholderPackageRevision
-	oldPackageRevision := &oldRev
-	oldPackageRevision.PrKey.WorkspaceName = "old"
-
-	newRev := *placeholderPackageRevision
-	newPackageRevision := &newRev
-	newPackageRevision.PrKey.WorkspaceName = "new"
-
-	repo := &fake.Repository{
-		PackageRevisions: []repository.PackageRevision{
-			oldPackageRevision,
-			newPackageRevision,
-			placeholderPackageRevision,
-		},
-	}
-	repoOpener := &fakeRepositoryOpener{
-		repository: repo,
-	}
-
-	mutation := upgradePackageMutation{
-		upgradeTask: &porchapi.Task{
-			Type: porchapi.TaskTypeUpgrade,
-			Upgrade: &porchapi.PackageUpgradeTaskSpec{
-				OldUpstream: porchapi.PackageRevisionRef{
-					Name: "repo.1234567890.old",
-				},
-				NewUpstream: porchapi.PackageRevisionRef{
-					Name: "repo.1234567890.new",
-				},
-				LocalPackageRevisionRef: porchapi.PackageRevisionRef{
-					Name: placeholderPackageName,
-				},
-			},
-		},
-
-		namespace:         "test-namespace",
-		pkgName:           pkg,
-		referenceResolver: res,
-		repoOpener:        repoOpener,
-	}
-	_, _, err := mutation.apply(context.Background(), repository.PackageResources{})
-	assert.ErrorContains(t, err, "placeholder package revision", "Expected error upgrading the placeholder package revision")
-
-	mutation.referenceResolver = (&errorAfterNReferenceResolver{r: res, err: errors.New("error resolving reference")}).startAt(4)
-
-	_, _, err = mutation.apply(context.Background(), repository.PackageResources{})
-	assert.ErrorContains(t, err, "failed to resolve repository reference")
-
-	mutation.referenceResolver = res
 }
