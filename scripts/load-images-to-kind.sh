ff5b70a7eaa460576b4f6e2bb5011b72a7a83bbe
Kushal Harish Naidu <kushal.harish.naidu@ericsson.com>
2026-08-21 10:54:22 +0100

    Fix not found error because informer cache lag on pod creation
    Signed-off-by: Kushal Harish Naidu <kushal.harish.naidu@ericsson.com>

--- scripts/load-images-to-kind.sh
+++ scripts/load-images-to-kind.sh
@@ -29,13 +29,12 @@ echo "Loading images to kind cluster..."
 if [ "${SKIP_IMG_BUILD}" = "false" ]; then
 
     # Build and load function runner if needed
-    if ! docker exec "${KIND_CONTEXT_NAME}-control-plane" crictl images | grep -q "${IMAGE_REPO}/${PORCH_FUNCTION_RUNNER_IMAGE}  *${IMAGE_TAG} "; then
+    if [ "${SKIP_FUNCTION_RUNNER_BUILD}" = "false" ]; then
         echo "Building ${IMAGE_REPO}/${PORCH_FUNCTION_RUNNER_IMAGE}:${IMAGE_TAG}"
         IMAGE_NAME="${PORCH_FUNCTION_RUNNER_IMAGE}" WRAPPER_SERVER_IMAGE_NAME="${PORCH_WRAPPER_SERVER_IMAGE}" IMAGE_REPO="${IMAGE_REPO}" IMAGE_TAG="${IMAGE_TAG}" make -C func/ build-image
         kind load docker-image ${IMAGE_REPO}/${PORCH_FUNCTION_RUNNER_IMAGE}:${IMAGE_TAG} -n ${KIND_CONTEXT_NAME}
         kind load docker-image ${IMAGE_REPO}/${PORCH_WRAPPER_SERVER_IMAGE}:${IMAGE_TAG} -n ${KIND_CONTEXT_NAME}
-    else
-        echo "Skipping building ${IMAGE_REPO}/${PORCH_FUNCTION_RUNNER_IMAGE}:${IMAGE_TAG} as it is already loaded into kind"
+        kubectl delete deployment -n porch-system --ignore-not-found=true function-runner
     fi
 
     # Build and load porch server if needed
